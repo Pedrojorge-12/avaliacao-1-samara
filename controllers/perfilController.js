8eaa05c3c98162b0a1dca0e15b11c375226f73eb
Athirson <athirsondev@gmail.com>
2026-03-21 15:22:32 -0300

    feat: Validações do perfilController

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -1,50 +1,103 @@
 const Perfil = require("../models/perfil")
-const Aluno = require("../models/aluno")
+const Aluno = require("../models/aluno");
+const errorResponse = require("../utils/errorResponse");
 
 const criarPerfil = async (req, res) => {
-  const { matricula, telefone, endereco, alunoId } = req.body;
+  try {
+    const { matricula, telefone, endereco, alunoId } = req.body;
 
-  const novoPerfil = new Perfil({
-    matricula,
-    telefone,
-    endereco,
-    aluno: alunoId,
-  });
+    if (!matricula || !alunoId) {
+      return errorResponse(res, "Matrícula e alunoId são obrigatórios", 400);
+    }
 
-  await novoPerfil.save();
+    const aluno = await Aluno.findById(alunoId);
+    if (!aluno) {
+      return errorResponse(res, "Aluno não encontrado", 404);
+    }
 
-  await Aluno.updateOne(
-    { _id: alunoId },
-    { $set: { perfil: novoPerfil._id } }
-  );
+    const novoPerfil = new Perfil({
+      matricula,
+      telefone,
+      endereco,
+      aluno: alunoId,
+    });
 
-  res.json({
-    message: "Perfil criado com sucesso!",
-    perfil: novoPerfil,
-  });
+    await novoPerfil.save();
+
+    await Aluno.updateOne(
+      { _id: alunoId },
+      { $set: { perfil: novoPerfil._id } }
+    );
+
+    return res.status(201).json({
+      message: "Perfil criado com sucesso!",
+      perfil: novoPerfil,
+    });
+  } catch (error) {
+    return errorResponse(res);
+  }
 };
 
 const obterTodosPerfis = async (req, res) => {
-  const perfis = await Perfil.find().populate('aluno');
-  res.json(perfis);
+  try {
+    const perfis = await Perfil.find().populate("aluno");
+    return res.status(200).json(perfis);
+  } catch (error) {
+    return errorResponse(res);
+  }
 };
 
 const deletarPerfil = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  await Perfil.deleteOne({ _id: id });
-  res.json({ message: "Perfil removido com sucesso!" });
+    const resultado = await Perfil.deleteOne({ _id: id });
+
+    if (resultado.deletedCount === 0) {
+      return errorResponse(res, "Perfil não encontrado", 404);
+    }
+
+    await Aluno.updateOne({ perfil: id }, { $unset: { perfil: "" } });
+
+    return res.status(200).json({ message: "Perfil removido com sucesso!" });
+  } catch (error) {
+    return errorResponse(res);
+  }
 };
 
 const editarPerfil = async (req, res) => {
-  const { id } = req.params;
-  const { matricula, telefone, endereco, alunoId } = req.body;
+  try {
+    const { id } = req.params;
+    const { matricula, telefone, endereco, alunoId } = req.body;
 
-  let perfil = await Perfil.findByIdAndUpdate(id, { matricula, telefone, endereco, aluno: alunoId });
-  res.status(200).json({
-    message: "Perfil atualizado com sucesso!",
-    perfil,
-  });
+    if (!matricula || !alunoId) {
+      return errorResponse(res, "Matrícula e alunoId são obrigatórios", 400);
+    }
+
+    const perfilAntigo = await Perfil.findById(id);
+    if (!perfilAntigo) {
+      return errorResponse(res, "Perfil não encontrado", 404);
+    }
+
+    const alunoAntigoId = perfilAntigo.aluno?.toString();
+    if (alunoAntigoId && alunoAntigoId !== alunoId) {
+      await Aluno.updateOne({ _id: alunoAntigoId }, { $unset: { perfil: "" } });
+      await Aluno.updateOne({ _id: alunoId }, { $set: { perfil: id } });
+    }
+
+    const perfil = await Perfil.findByIdAndUpdate(
+      id,
+      { matricula, telefone, endereco, aluno: alunoId },
+      { new: true }
+    );
+
+    return res.status(200).json({
+      message: "Perfil atualizado com sucesso!",
+      perfil,
+    });
+  } catch (error) {
+    return errorResponse(res);
+  }
 };
 
 module.exports = { criarPerfil, deletarPerfil, editarPerfil, obterTodosPerfis }
