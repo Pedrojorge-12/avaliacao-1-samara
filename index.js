e9e002ff58cc5b74ab126c48dbe61e43f27447d2
Pedrojorge-12 <ppjorge11@gmail.com>
2026-04-12 23:23:50 -0300

    cors liberado

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, '.')));
